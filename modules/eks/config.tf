dcc889c496266b55dce3bd7a18a3f7655645f0bd
Wesley Charles Blake <wesley@nikordi.com>
2019-08-16 14:28:58 +0200

    better naming of contexts

--- modules/eks/config.tf
+++ modules/eks/config.tf
@@ -22,21 +22,21 @@ clusters:
 - cluster:
     server: ${aws_eks_cluster.eks.endpoint}
     certificate-authority-data: ${aws_eks_cluster.eks.certificate_authority.0.data}
-  name: kubernetes
+  name: ${var.cluster-name}
 contexts:
 - context:
-    cluster: kubernetes
-    user: aws
-  name: aws
-current-context: aws
+    cluster: ${var.cluster-name}
+    user: ${var.cluster-name}
+  name: ${var.cluster-name}
+current-context: ${var.cluster-name}
 kind: Config
 preferences: {}
 users:
-- name: aws
+- name: ${var.cluster-name}
   user:
     exec:
       apiVersion: client.authentication.k8s.io/v1alpha1
-      command: aws-iam-authenticator
+      command: aws
       args:
         - "token"
         - "-i"
